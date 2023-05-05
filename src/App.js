ceaabdd14724444e6308337301798753bfae7ecd
sajad shokraei <shockzbeatbox@gmail.com>
2023-05-05 14:54:58 +0430

    main

---
+++ src/App.js
@@ -0,0 +1,45 @@
+
+import { Download, Features, SectionWrapper } from './components';
+import assets from './assets';
+import styles from './styles/Global';
+
+const App = () => {
+  return (
+    <>
+      <SectionWrapper 
+        title="My  React Native project Showcase "
+        description="You can use the bellow button to open myy app and in-order to use it you should have expo installed in your phone ."
+        showBtn
+        mockupImg={assets.homeHero}
+        banner="banner"
+      />
+      <SectionWrapper 
+        title="Smart User Interface Marketplace"
+        description="Experience a buttery UI of ProNef NFT Marketplace. Smooth constant colors of a fluent UI design."
+        mockupImg={assets.homeCards}
+        reverse
+      />
+      <Features />
+      <SectionWrapper 
+        title="Deployment"
+        description="ProNef is built using Expo which runs natively on all users' devices. You can easily get your app into people's hands"
+        mockupImg={assets.feature}
+        reverse
+      />
+      <SectionWrapper 
+        title="Creative way to showcase the store"
+        description="The app contains two screens. The first screen lists all NFTs while the second one shows the details of a specific NFT."
+        mockupImg={assets.mockup}
+        banner="banner02"
+      />
+      <Download />
+
+      <div className="px-4 py-2 justify-center items-center bg-primary flex-col text-center banner04 h-12">
+        <p className={`${styles.pText} ${styles.blackText}`}>made by Sajadzshoki
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
